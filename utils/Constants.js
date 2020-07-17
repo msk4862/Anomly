1b848325e012c24ca42102a8bea9f0f512ade927
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-07-17 20:43:53 +0530

    node export fix

--- utils/Constants.js
+++ utils/Constants.js
@@ -1,17 +1,21 @@
-export const APP_NAME = "Anomly";
-export const DESCRIPTION = "Now chat anonymously with anyone";
+module.exports = {
+    APP_NAME: "Anomly",
+    DESCRIPTION: "Now chat anonymously with anyone",
 
-export const SOCAIL_LINKS = {
-    github: "https://github.com/msk4862/Anomly",
-    linkedin: "https://www.linkedin.com/in/msk4862/",
-};
+    SOCAIL_LINKS: {
+        github: "https://github.com/msk4862/Anomly",
+        linkedin: "https://www.linkedin.com/in/msk4862/",
+    },
 
-export const SOCKET_MESSAGES = {
-    welcome: "A user has joined the room",
-    disconnect: "A user left has let the room",
-};
+    // SOCKET
+    SOCKET_MESSAGES: {
+        WELCOME: "Welcome to Anomly!",
+        JOINED: "A user has joined the room",
+        DISCONNECED: "A user left has let the room",
+    },
 
-export const SOCKET_EVENTS = {
-    MESSAGE: "message",
-    CHAT_MESSAGE: "chatMessage",
+    SOCKET_EVENTS: {
+        EVENT: "event",
+        CHAT_MESSAGE: "chatMessage",
+    },
 };
