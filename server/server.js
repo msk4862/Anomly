8bda83b8f133133b4d1aa84360d6bbdef9c0f595
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-07-25 22:18:14 +0530

    server util refraction

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const dev = process.env.NODE_ENV != "production";
 const nextApp = next({ dev });
 const nextHandler = nextApp.getRequestHandler();
 const { SOCKET_EVENTS, SOCKET_MESSAGES } = require("../utils/Constants");
-const formatMessage = require("../utils/MessageUtil");
+const formatMessage = require("./utils/MessageUtil");
 const { addUser, getCurrentUser } = require("./user");
 
 const PORT = 3000 || process.env.PORT;
