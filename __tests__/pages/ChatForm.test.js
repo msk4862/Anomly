d4ea11ffdabdd76472192c7a58773071f381348e
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-08-12 18:07:35 +0530

    form test updates

--- __tests__/pages/ChatForm.test.js
+++ __tests__/pages/ChatForm.test.js
@@ -4,54 +4,82 @@ import ChatForm from "../../components/Chat/ChatForm";
 
 afterEach(cleanup);
 
-test("Testing chat form with valid inputs", () => {
-    const onJoin = jest.fn();
-    const testUser = {
-        username: "Test123",
-        room: "test",
-    };
+describe("Chat form tests", () => {
+    test("Testing chat form with valid inputs", () => {
+        const onJoin = jest.fn();
+        const testUser = {
+            username: "Test123",
+            room: "test",
+        };
 
-    const { container, getByText } = render(<ChatForm handleSubmit={onJoin} />);
+        const { container, getByText } = render(
+            <ChatForm handleSubmit={onJoin} />
+        );
 
-    const usernameInput = container.querySelectorAll("input")[0];
-    const roomnameInput = container.querySelectorAll("input")[1];
-    const submitInput = getByText("Join Chat");
+        const usernameInput = container.querySelectorAll("input")[0];
+        const roomnameInput = container.querySelectorAll("input")[1];
+        const submitInput = getByText("Join Chat");
 
-    // setting value
-    fireEvent.change(usernameInput, { target: { value: testUser.username } });
-    fireEvent.change(roomnameInput, { target: { value: testUser.room } });
+        // setting value
+        fireEvent.change(usernameInput, {
+            target: { value: testUser.username },
+        });
+        fireEvent.change(roomnameInput, { target: { value: testUser.room } });
 
-    fireEvent.click(submitInput);
+        fireEvent.click(submitInput);
 
-    // Assertions
-    expect(onJoin).toHaveBeenCalledTimes(1);
-    expect(onJoin).toHaveBeenCalledWith(testUser);
-});
+        // Assertions
+        expect(onJoin).toHaveBeenCalledTimes(1);
+        expect(onJoin).toHaveBeenCalledWith(testUser);
+    });
 
-test("Testing chat form with invalid inputs", () => {
-    const onJoin = jest.fn();
-    var username = "";
-    var roomname = "";
+    test("Testing chat form with invalid inputs", () => {
+        const onJoin = jest.fn();
+        var username = "";
+        var roomname = "";
 
-    const { container, getByText } = render(<ChatForm handleSubmit={onJoin} />);
+        const { container, getByText } = render(
+            <ChatForm handleSubmit={onJoin} />
+        );
 
-    const usernameInput = container.querySelectorAll("input")[0];
-    const roomnameInput = container.querySelectorAll("input")[1];
-    const submitInput = getByText("Join Chat");
+        const usernameInput = container.querySelectorAll("input")[0];
+        const roomnameInput = container.querySelectorAll("input")[1];
+        const submitInput = getByText("Join Chat");
 
-    // setting value
-    fireEvent.change(usernameInput, { target: { value: username } });
-    fireEvent.change(roomnameInput, { target: { value: roomname } });
+        // setting value
+        fireEvent.change(usernameInput, { target: { value: username } });
+        fireEvent.change(roomnameInput, { target: { value: roomname } });
 
-    fireEvent.click(submitInput);
+        fireEvent.click(submitInput);
 
-    const usernameErrorMessage = getByText(/Username can't be empty!/i); //substring match, ignore case
-    const roomErrorMessage = getByText(/Room name can't be empty!/i);
-    const errorDivs = container.getElementsByClassName("error");
+        var usernameErrorMessage = getByText(/Username can't be empty!/i); //substring match, ignore case
+        var roomErrorMessage = getByText(/Room name can't be empty!/i);
+        var errorDivs = container.getElementsByClassName("error");
 
-    // Assertions
-    expect(onJoin).toHaveBeenCalledTimes(0);
-    expect(usernameErrorMessage).toBeInTheDocument();
-    expect(roomErrorMessage).toBeInTheDocument();
-    expect(errorDivs).toHaveLength(2);
+        // Assertions
+        expect(onJoin).toHaveBeenCalledTimes(0);
+        expect(usernameErrorMessage).toBeInTheDocument();
+        expect(roomErrorMessage).toBeInTheDocument();
+        expect(errorDivs).toHaveLength(2);
+
+        // testing other errors
+        username = "less";
+        roomname = "with space";
+
+        // setting value
+        fireEvent.change(usernameInput, { target: { value: username } });
+        fireEvent.change(roomnameInput, { target: { value: roomname } });
+
+        fireEvent.click(submitInput);
+
+        usernameErrorMessage = getByText(/Username is too short/i);
+        roomErrorMessage = getByText(/Room name can't have spaces/i);
+        errorDivs = container.getElementsByClassName("error");
+
+        // Assertions
+        expect(onJoin).toHaveBeenCalledTimes(0);
+        expect(usernameErrorMessage).toBeInTheDocument();
+        expect(roomErrorMessage).toBeInTheDocument();
+        expect(errorDivs).toHaveLength(2);
+    });
 });
