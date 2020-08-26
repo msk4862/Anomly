95363bbd61c2e1077415b3c79814747afe5063ac
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-08-26 13:27:35 +0530

    message syles updates

--- components/Chat/ChatMessage.js
+++ components/Chat/ChatMessage.js
@@ -1,10 +1,22 @@
+import { useContext } from "react";
+import { UserContext } from "./ChatHome";
 import { SOCKET_EVENTS } from "../../utils/Constants";
 import "../../styles/chatmessage.scss";
 
 const ChatMessage = ({ message }) => {
+    const user = useContext(UserContext);
+
     const renderChatMessage = () => {
-        return (
-            <div className="chat-message">
+        return user.username === message.user ? (
+            <div className="message message-chat message-chat-you tri-right">
+                <p>
+                    <small>You:</small>
+                    <small>{message.time}</small>
+                </p>
+                <p>{message.text}</p>
+            </div>
+        ) : (
+            <div className="message message-chat tri-left">
                 <p>
                     <small>{message.user}</small>
                     <small>{message.time}</small>
@@ -16,7 +28,7 @@ const ChatMessage = ({ message }) => {
 
     const renderEventMessage = () => {
         return (
-            <div className="bot-message text-center">
+            <div className="message message-bot text-center">
                 <p>{message.text}</p>
             </div>
         );
